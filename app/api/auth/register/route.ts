813464916b904d3341f1ff7aa25268fb92da5172
mishraji4325 <sonumishra4325@gmail.com>
2026-01-20 22:19:06 +0530

    First commit

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,37 @@
+import { connectToDatabase } from "@/lib/db";
+import User from "@/models/User";
+import { NextResponse, NextRequest } from "next/server";
+
+export async function POST(request: NextRequest){
+    try{
+        const {email, password} = await request.json()
+
+        if (!email || !password){
+            return NextResponse.json({
+                error: "Email and password are required"
+            },{status:400})
+        }
+
+        const userExists = await User.findOne({email})
+        if(userExists){
+            return NextResponse.json({
+                error: "User already exists"
+            },{status:400})
+        }
+
+        const user = await User.create({
+            email,
+            password
+        })
+        return NextResponse.json(
+            {message: "User registered successfully", userId: user._id},
+            {status:201},
+        )
+    }catch (error){
+        console.error("Error registering user:", error);
+        return NextResponse.json(
+            {error: "Something went wrong"}, 
+            {status:500}
+        )
+    }
+}
